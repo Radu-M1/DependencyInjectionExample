991b7565511a673e1857193ae9f76fccf26703c3
Ducul <rmbasceanu@gmail.com>
2025-10-31 18:20:48 +0200

    initial setup 2

---
+++ Services/CitiesService.cs
@@ -0,0 +1,43 @@
+﻿namespace Services;
+
+public class CitiesService
+{
+    private List<string> _cities;
+
+    public CitiesService()
+    {
+        _cities = new List<string>
+        {
+            "New York",
+            "Los Angeles",
+            "Chicago",
+            "Houston",
+            "Phoenix"
+        };
+    }
+
+    public List<string> GetCities()
+    {
+        return _cities;
+    }
+    public List<string> AddCity(string city)
+    {
+        _cities.Add(city);
+        return _cities;
+    }
+    public List<string> RemoveCity(string city)
+    {
+        _cities.Remove(city);
+        return _cities;
+    }
+
+    public List<string> UpdateCity(string oldCity, string newCity)
+    {
+        var index = _cities.IndexOf(oldCity);
+        if (index != -1)
+        {
+            _cities[index] = newCity;
+        }
+        return _cities;
+    }
+}
