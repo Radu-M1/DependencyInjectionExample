fee5740fd1d886ebb966070c7823bfb854eea53f
Ducul <rmbasceanu@gmail.com>
2025-11-01 19:33:08 +0200

    Before Dependency Injection

--- DependencyInjectionExampleApp/Controllers/HomeController.cs
+++ DependencyInjectionExampleApp/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Services;
 
 namespace DependencyInjectionExample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly CitiesService _citiesService;
+
+        public HomeController()
+        {
+            _citiesService = new CitiesService(); // here you must use Dependency Injection instead
+        }
         [Route("/")]
         public IActionResult Index()
         {
-            return View();
+            List<string> cities = _citiesService.GetCities();
+            // ViewBag.Cities = cities;
+            return View(cities);
         }
     }
 }
